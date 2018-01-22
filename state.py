b9ed900ccfb6ea3a789c6be2125c3fa367ac03f7
Zach Brogan <zachbrogan@gmail.com>
2018-01-22 15:19:20 -0700

    Change to temperature now query-based instead of SNS event based for simplicity between APIs. Add updater.py for retreiving temp and updating hvac using thermostat logic.

---
+++ state.py
@@ -0,0 +1,179 @@
+import boto3
+import os
+import json
+from uuid import uuid4 as Uuid
+import decimal
+
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+
+def state_table():
+    return boto3.resource('dynamodb').Table(os.environ['STATE_TABLE_NAME'])
+
+
+def get_thermostat(uuid):
+    thermostat = state_table().get_item(Key={'uuid': uuid})
+    response = {
+        'isBase64Encoded': 'false',
+        'statusCode': 200,
+        'body': json.dumps(thermostat['Item'], cls=DecimalEncoder)
+    }
+    return response
+
+
+def get_all_thermostats():
+    thermostats = state_table().scan()
+    response = {
+        'isBase64Encoded': 'false',
+        'statusCode': 200,
+        'body': json.dumps(thermostats['Items'], cls=DecimalEncoder)
+    }
+    return response
+
+
+def create_thermostat(thermostat_data):
+    if (not thermostat_data['area'] or not
+            isinstance(thermostat_data['area'], str)):
+        raise Exception('Error. Must specify area.')
+    if (not thermostat_data['hvac_url'] or not
+            isinstance(thermostat_data['hvac_url'], str)):
+        raise Exception('Error. Must specify hvac_url.')
+    if (not thermostat_data['temperature_url'] or not
+            isinstance(thermostat_data['temperature_url'], str)):
+        raise Exception('Error. Must specify temperature_url.')
+
+    uuid = Uuid().hex
+    state = {
+        'uuid': uuid,
+        'area': thermostat_data['area'],
+        'hvac_url': thermostat_data['hvac_url'],
+        'temperature_url': thermostat_data['temperature_url'],
+        'desired_temperature': 70,
+        'eco_min_temperature': 65,
+        'eco_max_temperature': 78,
+        'tolerance': 1,
+        'mode': 'normal',
+        'fan': 'auto'
+    }
+    state_table().put_item(Item=state)
+    response = {
+        'isBase64Encoded': 'false',
+        'statusCode': 200,
+        'body': json.dumps(state)
+    }
+    return response
+
+
+def update_thermostat(uuid, thermostat_data):
+    updateExpressions = []
+    attributeValues = {}
+    if 'area' in thermostat_data and isinstance(thermostat_data['area'], str):
+        updateExpressions.append("area = :a")
+        attributeValues[':a'] = thermostat_data['area']
+    if ('hvac_url' in thermostat_data and
+            isinstance(thermostat_data['hvac_url'], str)):
+        updateExpressions.append("hvac_url = :h")
+        attributeValues[':h'] = thermostat_data['hvac_url']
+    if ('temperature_url' in thermostat_data and
+            isinstance(thermostat_data['temperature_url'], str)):
+        updateExpressions.append("temperature_url = :t")
+        attributeValues[':t'] = thermostat_data['temperature_url']
+    if ('desired_temperature' in thermostat_data and
+            isinstance(thermostat_data['desired_temperature'], int)):
+        updateExpressions.append("desired_temperature = :d")
+        attributeValues[':d'] = thermostat_data['desired_temperature']
+    if ('eco_min_temperature' in thermostat_data and
+            isinstance(thermostat_data['eco_min_temperature'], int)):
+        updateExpressions.append("eco_min_temperature = :e")
+        attributeValues[':e'] = thermostat_data['eco_min_temperature']
+    if ('eco_max_temperature' in thermostat_data and
+            isinstance(thermostat_data['eco_max_temperature'], int)):
+        updateExpressions.append("eco_max_temperature = :x")
+        attributeValues[':x'] = thermostat_data['eco_max_temperature']
+    if ('tolerance' in thermostat_data and
+            isinstance(thermostat_data['tolerance'], int)):
+        updateExpressions.append("tolerance = :l")
+        attributeValues[':l'] = thermostat_data['tolerance']
+    if 'mode' in thermostat_data and isinstance(thermostat_data['mode'], str):
+        updateExpressions.append("mode = :m")
+        attributeValues[':m'] = thermostat_data['mode']
+    if 'fan' in thermostat_data and isinstance(thermostat_data['fan'], str):
+        updateExpressions.append("fan = :f")
+        attributeValues[':f'] = thermostat_data['fan']
+
+    if len(updateExpressions) < 1:
+        raise Exception('Error. Invalid update request.')
+    updateExpressionStr = "set " + (",".join(updateExpressions))
+
+    therm = state_table().update_item(
+        Key={'uuid': uuid},
+        UpdateExpression=updateExpressionStr,
+        ExpressionAttributeValues=attributeValues,
+        ReturnValues="ALL_NEW")
+
+    # This method will call the updater lambda
+    boto3.client('lambda').invoke(
+        FunctionName=os.environ['UPDATER_FUNCTION_NAME'],
+        InvocationType='Event')  # Add payload (therm)
+
+    response = {
+        "isBase64Encoded": "false",
+        "statusCode": 200,
+        "body": "{\"message\": \"Hvac updated\"}"
+    }
+    return response
+
+
+def delete_thermostat(uuid):
+    # Delete thermostat state
+    state_table().delete_item(Key={'uuid': uuid})
+    response = {
+        "isBase64Encoded": "false",
+        "statusCode": 200,
+        "body": "{\"message\": \"Hvac deleted.\"}"
+    }
+    return response
+
+
+def lambda_handler(event, context):
+    try:
+        if event['httpMethod'] == "GET":
+            if event['pathParameters'] and 'uuid' in event['pathParameters']:
+                return get_thermostat(event['pathParameters']['uuid'])
+            else:
+                return get_all_thermostats()
+
+        elif event['httpMethod'] == "POST":
+            if event['body'] and not event['pathParameters']:
+                return create_thermostat(json.loads(event['body']))
+            else:
+                raise Exception(
+                    'Error. HTTP body required for POST to create thermostat.')
+
+        elif event['httpMethod'] == "PUT":
+            if (event['body'] and event['pathParameters'] and
+                    'uuid' in event['pathParameters']):
+                return update_thermostat(event['pathParameters']['uuid'],
+                                         json.loads(event['body']))
+
+        elif event['httpMethod'] == "DELETE":
+            if event['pathParameters'] and 'uuid' in event['pathParameters']:
+                return delete_thermostat(event['pathParameters']['uuid'])
+        else:
+            raise Exception("Invalid HTTP method")
+    except Exception as e:
+        response = {
+            "isBase64Encoded": "false",
+            "statusCode": 400,
+            "body": "{\"errorMessage\": \"" + e.args[0] + ".\"}"
+        }
+        return response
---
+++ updater.py
@@ -0,0 +1,99 @@
+from urllib import request
+from state import *
+
+hvac_idle = {"heater": False, "ac": False, "fan": False}
+hvac_vent = {"heater": False, "ac": False, "fan": True}
+hvac_heat = {"heater": True, "ac": False, "fan": True}
+hvac_cool = {"heater": False, "ac": True, "fan": True}
+
+
+def send_next_hvac(hvac_url, hvac, next_hvac):
+    if next_hvac.items() <= hvac.items():
+        return
+    data = json.dumps(next_hvac).encode('utf8')
+    req = request.Request(
+        url=hvac_url, data=data, method='PUT')
+    resp = request.urlopen(req)
+    print(resp.getcode())
+
+
+def update_hvac(thermostat, off_time_increment):
+    hvac = json.loads(request.urlopen(thermostat['hvac_url']).read())
+    temp = json.loads(request.urlopen(thermostat['temperature_url']).read())
+    temperature = temp['temperature']
+    next_hvac = {}
+    desired = thermostat['desired_temperature']
+    eco_min = thermostat['eco_min_temperature']
+    eco_max = thermostat['eco_max_temperature']
+
+    if thermostat['mode'] == "off":
+        next_hvac = hvac_idle
+        next_hvac['off_time'] = hvac['off_time'] + off_time_increment
+    elif thermostat['mode'] == "eco":
+        # Too cold
+        if ((temperature < eco_min - thermostat['tolerance']) or
+                (temperature < eco_min and hvac['heater'])):
+            next_hvac = hvac_heat
+            next_hvac['off_time'] = hvac['off_time'] + off_time_increment
+        # Too hot
+        elif (hvac['off_time'] >= hvac['min_off_time'] and (
+                temperature > eco_max + thermostat['tolerance'] or (
+                temperature > eco_max and hvac['ac']))):
+            next_hvac = hvac_cool
+            next_hvac['off_time'] = 0
+        else:
+            next_hvac = hvac_idle
+            next_hvac['off_time'] = hvac['off_time'] + off_time_increment
+    elif thermostat['mode'] == "normal":
+        # Too cold
+        if ((temperature < desired - thermostat['tolerance']) or
+                (temperature < desired and hvac['heater'])):
+            next_hvac = hvac_heat
+            next_hvac['off_time'] = hvac['off_time'] + off_time_increment
+        # Too hot
+        elif (hvac['off_time'] >= hvac['min_off_time'] and (
+                temperature > desired + thermostat['tolerance'] or (
+                temperature > desired and hvac['ac']))):
+            next_hvac = hvac_cool
+            next_hvac['off_time'] = 0
+        else:
+            next_hvac = hvac_idle
+            next_hvac['off_time'] = hvac['off_time'] + off_time_increment
+    elif thermostat['mode'] == "heat":
+        # Too cold
+        if ((temperature < desired - thermostat['tolerance']) or
+                (temperature < desired and hvac['heater'])):
+            next_hvac = hvac_heat
+        # Too hot
+        else:
+            next_hvac = hvac_idle
+        next_hvac['off_time'] = hvac['off_time'] + off_time_increment
+    elif thermostat['mode'] == "cool":
+        # Too hot
+        if (hvac['off_time'] >= hvac['min_off_time'] and (
+                temperature > desired + thermostat['tolerance'] or (
+                temperature > desired and hvac['ac']))):
+            next_hvac = hvac_cool
+            next_hvac['off_time'] = 0
+        # Too cold
+        else:
+            next_hvac = hvac_idle
+            next_hvac['off_time'] = hvac['off_time'] + off_time_increment
+    else:
+        raise Exception("Invalid thermostat mode")
+
+    # Check fan
+    if thermostat['fan'] == 'on':
+        next_hvac['fan'] = True
+    send_next_hvac(thermostat['hvac_url'], hvac, next_hvac)
+
+
+def update_hvacs():
+    thermostats = state_table().scan()['Items']
+    for thermostat in thermostats:
+        update_hvac(thermostat, off_time_increment=1)
+
+
+def lambda_handler(event, context):
+    # Add logic to determine if job sent by Chron or HTTP event
+    update_hvacs()
